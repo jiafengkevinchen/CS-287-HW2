d95209418efd0cf87a94caf96d6d5d8afcd2481b
Francisco Rivera <frtennis1@gmail.com>
2019-02-13 19:10:15 -0500

    LSTM model

---
+++ models/lstm.py
@@ -0,0 +1,27 @@
+from namedtensor.nn import nn as nnn
+
+class LSTM(nnn.Module):
+    def __init__(self, TEXT, embedding_dim=100, hidden_dim=150):
+        super().__init__()
+        
+        pad_idx = TEXT.vocab.stoi['<pad>']
+        
+        self.embed = nnn.Embedding(num_embeddings=len(TEXT.vocab), 
+                                   embedding_dim=embedding_dim, 
+                                   padding_idx=pad_idx)
+        
+        self.lstm = nnn.LSTM(input_size=embedding_dim, 
+                             hidden_size=hidden_dim, 
+                             num_layers=1) \
+                        .spec("embedding", "seqlen")
+        
+        self.w = nnn.Linear(in_features=hidden_dim, 
+                            out_features=len(TEXT.vocab)) \
+                        .spec("embedding", "vocab")
+    
+    def forward(self, batch_text):
+        embedded = self.embed(batch_text)
+        hidden_states, _ = self.lstm(embedded)
+        log_probs = self.w(hidden_states)
+        
+        return log_probs
